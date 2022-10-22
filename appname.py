2ece5cf5b4f354a2cecab36ef35dc3909afb5133
Valdimar Ágúst Eggertsson <valdegg@gmail.com>
2022-10-22 18:36:44 +0200

    Update appname.py

--- appname.py
+++ appname.py
@@ -1,9 +1,14 @@
 
 import streamlit as st
 import json
+
 with open('vatnið_brennur_samheiti_nyjast.json', encoding='utf-8') as f2:
     vatnsam = json.load(f2)
+with open('vatnið_brennur_orðtíðni_2.json', encoding='utf-8') as f2:
+    vatnord = json.load(f2)
 
 #displaying JSON content
 
 st.json(vatnsam)
+
+st.json(vatnord)
